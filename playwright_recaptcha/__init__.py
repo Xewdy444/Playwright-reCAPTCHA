6ab399f70712ed188a4ea20cd4023d34b705e6cd
Xewdy <95155966+Xewdy444@users.noreply.github.com>
2023-01-19 17:50:49 -0600

    Added __all__ to all __init__.py files

--- playwright_recaptcha/__init__.py
+++ playwright_recaptcha/__init__.py
@@ -12,4 +12,17 @@ from playwright_recaptcha.recaptchav2.sync_solver import SyncSolver as SyncSolve
 from playwright_recaptcha.recaptchav3.async_solver import AsyncSolver as AsyncSolverV3
 from playwright_recaptcha.recaptchav3.sync_solver import SyncSolver as SyncSolverV3
 
+__all__ = [
+    "RecaptchaError",
+    "RecaptchaNotFoundError",
+    "RecaptchaRateLimitError",
+    "RecaptchaSolveError",
+    "RecaptchaTimeoutError",
+    "RecaptchaVersionError",
+    "AsyncSolverV2",
+    "SyncSolverV2",
+    "AsyncSolverV3",
+    "SyncSolverV3",
+]
+
 __version__ = "0.0.3"
--- playwright_recaptcha/recaptchav3/__init__.py
+++ playwright_recaptcha/recaptchav3/__init__.py
@@ -1,3 +1,5 @@
 """Recaptcha v3 solver for Playwright."""
 from playwright_recaptcha.recaptchav3.async_solver import AsyncSolver
 from playwright_recaptcha.recaptchav3.sync_solver import SyncSolver
+
+__all__ = ["AsyncSolver", "SyncSolver"]
