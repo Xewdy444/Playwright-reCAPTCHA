4c4d818cee980d1a726fd1e1b7869deb8bb24575
Xewdy <95155966+Xewdy444@users.noreply.github.com>
2023-01-26 12:29:43 -0600

    Corrected capitalization of reCAPTCHA

--- playwright_recaptcha/recaptchav2/__init__.py
+++ playwright_recaptcha/recaptchav2/__init__.py
@@ -1,4 +1,4 @@
-"""Recaptcha v2 solver for Playwright."""
+"""reCAPTCHA v2 solver for Playwright."""
 from playwright_recaptcha.recaptchav2.async_solver import AsyncSolver
 from playwright_recaptcha.recaptchav2.sync_solver import SyncSolver
 
--- playwright_recaptcha/recaptchav3/__init__.py
+++ playwright_recaptcha/recaptchav3/__init__.py
@@ -1,4 +1,4 @@
-"""Recaptcha v3 solver for Playwright."""
+"""reCAPTCHA v3 solver for Playwright."""
 from playwright_recaptcha.recaptchav3.async_solver import AsyncSolver
 from playwright_recaptcha.recaptchav3.sync_solver import SyncSolver
 
