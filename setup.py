2022db45b173ecc1ec148331133d93dfe9f50df3
Xewdy <95155966+Xewdy444@users.noreply.github.com>
2023-02-13 17:18:00 -0600

    Changed long description to be read via context manager

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import find_packages, setup
 
+with open("README.md", encoding="utf-8") as file:
+    long_description = file.read()
+
 setup(
     name="playwright-recaptcha",
     version="0.0.7",
@@ -7,7 +10,7 @@ setup(
     author_email="xewdy@xewdy.tech",
     description="A library for solving reCAPTCHA v2 and v3 with Playwright",
     license="MIT",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Xewdy444/Playwright-reCAPTCHA",
     packages=find_packages(),
