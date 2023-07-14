50626a11b2b933d9de79da50e8c81171f20df15b
Xewdy <95155966+Xewdy444@users.noreply.github.com>
2023-07-14 14:11:20 -0500

    Added message parameter for RecaptchaNotFoundError

--- playwright_recaptcha/errors.py
+++ playwright_recaptcha/errors.py
@@ -1,8 +1,22 @@
 from typing import Optional
 
 
+class CapSolverError(Exception):
+    """An exception raised when the CapSolver API returns an error."""
+
+    def __init__(self, message: Optional[str] = None) -> None:
+        super().__init__(message or "The CapSolver API returned an error.")
+
+
 class RecaptchaError(Exception):
     """Base class for reCAPTCHA exceptions."""
+
+
+class RecaptchaNotFoundError(RecaptchaError):
+    """An exception raised when the reCAPTCHA was not found."""
+
+    def __init__(self, message: Optional[str] = None) -> None:
+        super().__init__(message or "The reCAPTCHA was not found.")
 
 
 class RecaptchaSolveError(RecaptchaError):
@@ -10,13 +24,6 @@ class RecaptchaSolveError(RecaptchaError):
 
     def __init__(self, message: Optional[str] = None) -> None:
         super().__init__(message or "The reCAPTCHA could not be solved.")
-
-
-class RecaptchaNotFoundError(RecaptchaError):
-    """An exception raised when the reCAPTCHA was not found."""
-
-    def __init__(self) -> None:
-        super().__init__("The reCAPTCHA was not found.")
 
 
 class RecaptchaRateLimitError(RecaptchaSolveError):
@@ -31,10 +38,3 @@ class RecaptchaTimeoutError(RecaptchaSolveError):
 
     def __init__(self) -> None:
         super().__init__("The reCAPTCHA solve timeout has been exceeded.")
-
-
-class CapSolverError(Exception):
-    """An exception raised when the CapSolver API returns an error."""
-
-    def __init__(self, message: Optional[str] = None) -> None:
-        super().__init__(message or "The CapSolver API returned an error.")
