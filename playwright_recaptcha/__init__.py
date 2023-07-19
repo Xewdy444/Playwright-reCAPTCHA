cea3f388082b95c874223126269f3beba5aca118
Xewdy <95155966+Xewdy444@users.noreply.github.com>
2023-07-18 21:21:23 -0500

    Bump version to 0.3.0

--- playwright_recaptcha/__init__.py
+++ playwright_recaptcha/__init__.py
@@ -1,7 +1,7 @@
 """A library for solving reCAPTCHA v2 and v3 with Playwright."""
 
 __author__ = "Xewdy444"
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __license__ = "MIT"
 
 from playwright_recaptcha.errors import (
