c300ceff0f0d221b7c87351f5bb8c6da27ce3929
Xewdy444 <xewdy@xewdy.systems>
2024-05-18 13:16:37 -0500

    Added German translations

--- playwright_recaptcha/recaptchav2/translations.py
+++ playwright_recaptcha/recaptchav2/translations.py
@@ -5,6 +5,7 @@ ELEMENT_TRANSLATIONS = {
         "进行人机身份验证",
         "No soy un robot",
         "Je ne suis pas un robot",
+        "Ich bin kein Roboter",
     ),
     "get_an_audio_challenge": (
         "Get an audio challenge",
@@ -12,6 +13,7 @@ ELEMENT_TRANSLATIONS = {
         "改用音频验证",
         "Obtener una pista sonora",
         "Générer un test audio",
+        "Audio-Captcha abrufen",
     ),
     "get_a_visual_challenge": (
         "Get a visual challenge",
@@ -19,6 +21,7 @@ ELEMENT_TRANSLATIONS = {
         "改用图片验证",
         "Obtener una pista visual",
         "Générer un test visuel",
+        "Visuelles Captcha abrufen",
     ),
     "get_a_new_challenge": (
         "Get a new challenge",
@@ -26,6 +29,7 @@ ELEMENT_TRANSLATIONS = {
         "换一个新的验证码",
         "Obtener una pista nueva",
         "Générer un nouveau test",
+        "Neues Captcha abrufen",
     ),
     "alternatively_download_audio_as_mp3": (
         "Alternatively, download audio as MP3",
@@ -33,6 +37,7 @@ ELEMENT_TRANSLATIONS = {
         "或者以 MP3 格式下载音频",
         "También puedes descargar el audio en formato MP3",
         "Ou téléchargez le fichier audio au format MP3",
+        "Audio als MP3 herunterladen",
     ),
     "enter_what_you_hear": (
         "Enter what you hear",
@@ -40,16 +45,18 @@ ELEMENT_TRANSLATIONS = {
         "请输入您听到的内容",
         "Escribe lo que escuches",
         "Saisissez ce que vous entendez",
+        "Geben Sie ein, was Sie hören",
     ),
-    "skip": ("Skip", "Пропустить", "跳过", "Saltar", "Ignorer"),
-    "next": ("Next", "Далее", "下一个", "Siguiente", "Suivant"),
-    "verify": ("Verify", "Подтвердить", "验证", "Verificar", "Valider"),
+    "skip": ("Skip", "Пропустить", "跳过", "Saltar", "Ignorer", "Überspringen"),
+    "next": ("Next", "Далее", "下一个", "Siguiente", "Suivant", "Weiter"),
+    "verify": ("Verify", "Подтвердить", "验证", "Verificar", "Valider", "Bestätigen"),
     "try_again_later": (
         "Try again later",
         "Повторите попытку позже",
         "稍后重试",
         "Inténtalo de nuevo más tarde",
         "Réessayez plus tard",
+        "Später noch einmal versuchen",
     ),
     "multiple_correct_solutions_required": (
         "Multiple correct solutions required - please solve more",
@@ -57,6 +64,7 @@ ELEMENT_TRANSLATIONS = {
         "需要提供多个正确答案 - 请回答更多问题",
         "Debes resolver más captchas",
         "Veuillez effectuer d'autres tests (vous devez fournir plusieurs solutions correctes)",
+        "Es sind mehrere richtige Lösungen erforderlich. Bitte weitere Aufgaben lösen",
     ),
     "press_play_to_listen": (
         "Press PLAY to listen",
@@ -64,6 +72,7 @@ ELEMENT_TRANSLATIONS = {
         "按“播放”可听语音内容",
         "Pulsa REPRODUCIR para escuchar el audio",
         "Appuyez sur LECTURE pour écouter",
+        "Wählen Sie WIEDERGABE aus, um die Wiedergabe zu starten",
     ),
     "please_try_again": (
         "Please try again",
@@ -71,6 +80,7 @@ ELEMENT_TRANSLATIONS = {
         "请重试",
         "Inténtalo de nuevo",
         "Veuillez réessayer",
+        "Versuche es bitte erneut",
     ),
     "please_also_check_the_new_images": (
         "Please also check the new images",
@@ -78,6 +88,7 @@ ELEMENT_TRANSLATIONS = {
         "另外，您还需查看新显示的图片",
         "Comprueba también las imágenes nuevas",
         "Veuillez également vérifier les nouvelles images",
+        "Sehen Sie sich auch die neuen Bilder an",
     ),
     "please_select_all_matching_images": (
         "Please select all matching images",
@@ -85,15 +96,47 @@ ELEMENT_TRANSLATIONS = {
         "请选择所有相符的图片",
         "Selecciona todas las imágenes que coincidan",
         "Veuillez sélectionner toutes les images correspondantes",
+        "Wählen Sie alle passenden Bilder aus",
     ),
 }
 
 OBJECT_TRANSLATIONS = {
-    "taxis": ("taxis", "такси", "出租车"),
-    "bus": ("bus", "buses", "автобус", "автобусы", "公交车", "autobuses", "autobús"),
-    "motorcycles": ("motorcycles", "мотоциклы", "摩托车", "motocicletas", "motos"),
-    "tractors": ("tractors", "трактора", "拖拉机", "tractores", "tracteurs"),
-    "chimneys": ("chimneys", "дымовые трубы", "烟囱", "chimeneas", "cheminées"),
+    "taxis": ("taxis", "такси", "出租车", "Taxis"),
+    "bus": (
+        "bus",
+        "buses",
+        "автобус",
+        "автобусы",
+        "公交车",
+        "autobuses",
+        "autobús",
+        "Bus",
+        "Bussen",
+    ),
+    "motorcycles": (
+        "motorcycles",
+        "мотоциклы",
+        "摩托车",
+        "motocicletas",
+        "motos",
+        "Motorrädern",
+    ),
+    "tractors": (
+        "tractors",
+        "трактора",
+        "拖拉机",
+        "tractores",
+        "tracteurs",
+        "Traktoren",
+    ),
+    "chimneys": (
+        "chimneys",
+        "дымовые трубы",
+        "烟囱",
+        "chimeneas",
+        "cheminées",
+        "Schornsteine",
+    ),
     "crosswalks": (
         "crosswalks",
         "пешеходные переходы",
@@ -101,6 +144,7 @@ OBJECT_TRANSLATIONS = {
         "过街人行道",
         "pasos de peatones",
         "passages pour piétons",
+        "Fußgängerüberwegen",
     ),
     "traffic_lights": (
         "traffic lights",
@@ -108,25 +152,35 @@ OBJECT_TRANSLATIONS = {
         "红绿灯",
         "semáforos",
         "feux de circulation",
+        "Ampeln",
     ),
-    "bicycles": ("bicycles", "велосипеды", "自行车", "bicicletas", "vélos"),
+    "bicycles": (
+        "bicycles",
+        "велосипеды",
+        "自行车",
+        "bicicletas",
+        "vélos",
+        "Fahrrädern",
+    ),
     "parking_meters": (
         "parking meters",
         "парковочные автоматы",
         "停车计时器",
         "parquímetros",
         "parcmètres",
+        "Parkometern",
     ),
-    "cars": ("cars", "автомобили", "小轿车", "coches", "voitures"),
-    "bridges": ("bridges", "мостами", "桥", "puentes", "ponts"),
-    "boats": ("boats", "лодки", "船", "barcos", "bateaux"),
-    "palm_trees": ("palm trees", "пальмы", "棕榈树", "palmeras", "palmiers"),
+    "cars": ("cars", "автомобили", "小轿车", "coches", "voitures", "Pkws"),
+    "bridges": ("bridges", "мостами", "桥", "puentes", "ponts", "Brücken"),
+    "boats": ("boats", "лодки", "船", "barcos", "bateaux", "Boote"),
+    "palm_trees": ("palm trees", "пальмы", "棕榈树", "palmeras", "palmiers", "Palmen"),
     "mountains_or_hills": (
         "mountains or hills",
         "mountain",
         "горы или холмы",
         "montañas o colinas",
         "montagnes ou collines",
+        "Berge oder Hügel",
     ),
     "fire_hydrant": (
         "a fire hydrant",
@@ -138,8 +192,17 @@ OBJECT_TRANSLATIONS = {
         "una boca de incendios",
         "borne d'incendie",
         "bouches d'incendie",
+        "Hydranten",
+        "Feuerhydranten",
     ),
-    "stairs": ("stairs", "лестницы", "楼梯", "escaleras", "escaliers"),
+    "stairs": (
+        "stairs",
+        "лестницы",
+        "楼梯",
+        "escaleras",
+        "escaliers",
+        "Treppen(stufen)",
+    ),
 }
 
-ORIGINAL_LANGUAGE_AUDIO = ("es", "fr")
+ORIGINAL_LANGUAGE_AUDIO = ("de", "es", "fr")
